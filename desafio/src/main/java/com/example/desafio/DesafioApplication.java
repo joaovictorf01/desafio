f52885260c262346f0b2d9223353f05611b39e0e
joão victor <joaovictorf01@gmail.com>
2022-08-09 05:05:02 -0300

    adicionando um hello para testar.

--- desafio/src/main/java/com/example/desafio/DesafioApplication.java
+++ desafio/src/main/java/com/example/desafio/DesafioApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DesafioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DesafioApplication.class, args);
 	}
 
 }
--- desafio/src/test/java/com/example/desafio/DesafioApplicationTests.java
+++ desafio/src/test/java/com/example/desafio/DesafioApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.desafio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DesafioApplicationTests {
 
 	@Test
 	void contextLoads() {
