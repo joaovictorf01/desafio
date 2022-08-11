c5cd5067d753bc87b642c73832965d725839c90e
joão victor <joaovictorf01@gmail.com>
2022-08-11 07:13:07 -0300

    refatorando o projeto para c#

---
+++ desafio/Models/Receita.cs
@@ -0,0 +1,10 @@
+namespace desafio.Models;
+
+public class Receita
+{
+    public string descricao { get; set; }
+    public double valor { get; set; }
+    public int id { get; set; }
+    public DateTime data { get; set; }
+
+}
